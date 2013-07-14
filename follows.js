4d555fd6470f93f22446ebd0758d7165d6224351
simtschitsch <harri_s@gmx.net>
2013-07-14 12:44:25 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,8 +12,8 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
+	var defaultKey		= '3SarHcD0AV8qAe6wsrcw6v8tIjL4UmVNF0S5nnzaumurE0MS', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [85178], // Comma separated array of Xively Feed ID numbers
 		applicationName	= '', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
