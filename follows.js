246f1703f395270105af4afd06f24c705871b915
simtschitsch <harri_s@gmx.net>
2013-07-21 13:36:47 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -88,7 +88,7 @@
 					 if(duration == '1week') diff = 604800000;
 					 if(duration == '1month') diff = 2628000000;
 					 if(duration == '90days') diff = 7884000000;
-					then.setTime(now.getTime()+7200000 - diff);
+					then.setTime(now.getTime() - diff);
 					if(updated.getTime() > then.getTime()) {
 						if(datastreamIds && datastreamIds != '' && datastreamIds.indexOf(datastream.id) >= 0) {
 							xively.datastream.history(feedId, datastream.id, {duration: duration, interval: interval, limit: 1000}, function(datastreamData) {
@@ -130,7 +130,7 @@
 
 									// Add Each Datapoint to Array
 									datastreamData.datapoints.forEach(function(datapoint) {
-										points.push({x: new Date(datapoint.at).getTime()/1000.0, y: parseFloat(434-datapoint.value)});
+										points.push({x: new Date((datapoint.at).getTime()+7200000)/1000.0, y: parseFloat(434-datapoint.value)});
 									});
 
 									// Add Datapoints Array to Graph Series Array
